28bbbf5d3d65927506ca57d7d90d8dfa724066a8
RonakNandanwar26 <ronaknandanwar10@gmail.com>
2021-03-08 17:37:26 +0530

    email added in setup file

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"ronaknandanwar10@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
